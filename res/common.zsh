2c2e77f2dc36bf89f319bf7aa6057a5a7f8e5353
BarryLhm <BarryLhm@outlook.com>
2025-10-01 19:13:20 +0800

    final: moving to zsh & add lang

--- res/common.zsh
+++ res/common.zsh
@@ -1,4 +1,5 @@
-#!/bin/bash ## not executable, just for syntax highlighting
+#!/bin/zsh
+## not executable, just for syntax highlighting
 
 set -eu
 
@@ -20,10 +21,12 @@ R_DIR=/runtime
 D_NULL=/dev/null
 X_DISPLAY=:3
 
-SCRIPT="$(realpath $0)"
+## original $0 to be passed from caller
+SCRIPT="$(realpath $1)"
 PROG=${1##*/}
 
-[ ${RUNTIME_DIR-} ] && DIR=$RUNTIME_DIR || DIR=${${"$(realpath $0)"%/*}%/*}
+COMMON="$(realpath $0)"
+DIR=${${COMMON%/*}%/*}
 RES=$DIR/res
 DATA=$DIR/data
 LANG_D=$RES/lang
@@ -32,6 +35,7 @@ CONF_D=$DATA/conf.d
 ### configurable
 C_ROOT=$DATA/rootfs
 OPU_DIR=$DATA/openutau
+HOME_DIR=$DATA/home
 O_DATA=/sdcard/OpenUtau
 ###
 #. $CONF_D/default.conf
@@ -40,6 +44,7 @@ DB_MIRROR=https://mirrors.ustc.edu.cn/ubuntu-ports
 DB_SUITE=plucky
 DEFAULT_COLOR=GREEN
 
+declare -A MESSAGES=() ## fallback
 declare -A T_COLOR=(
 	[RESET]='\e[0m' [UNDERLINE]='\e[4m' [BLINK]='\e[5m'
 	[WHITE]='\e[37m\e[1m' [BLACK]='\e[30m' [GRAY]='\e[30m\e[1m'
@@ -48,11 +53,21 @@ declare -A T_COLOR=(
 )
 
 # gettext
-_C()
+M()
 {
 	local key=$1; shift
-	printf $M[$key] $@
+	printf ${MESSAGES[$key]-untranslated."$key" %s %s %s %s %s %s} $@
+}
 
+load_lang()
+{
+	export LANG=$1
+	LANG_F=$LANG_D/$LANG.lang
+	[ -f $LANG_F ] && . $LANG_F || \
+	{
+		msg "Unknown Language: $LANG"
+		load_lang en_US.UTF-8
+	}
 }
 
 msg() #(string message, [T_COLOR[] colors])
@@ -66,85 +81,85 @@ msg() #(string message, [T_COLOR[] colors])
 
 error() #(string message, [int retval])
 {
-	msg "[$(_C error)] $1" RED
-	return "${2-1}"
+	msg "[$(M error)] $1" RED
+	return ${2-1}
 }
 
 errexit() # same as error
 {
-	error "$@" || exit "$?"
+	error $@ || exit $?
 }
 
 createdir() #(path[] dirs)
 {
 	local i
-	for i in "$@"
-	do	[ -d "$i" ] || mkdir -- "$i" || error "无法创建目录：'$i'，请手动处理"
+	for i in $@
+	do	[ -d $i ] || mkdir -- $i || error "$(M mkdir.failed $i)"
 	done
 }
 
 print_run() #(string[] cmdline)
 {
-	msg "[运行命令] $*" GREEN
-	"$@"
+	msg "[$(M runcmd)] $*" GREEN
+	$@
 }
 
 package() #(operation, [string[] args])
 {
-	local oper="$1"; shift
-	case "$oper" in
+	local oper=$1; shift
+	case $oper in
 	sync) print_run apt-get update;;
-	add) print_run apt-get install -y "$@";;
+	add) print_run apt-get install -y $@;;
 	up) print_run apt-get upgrade -y;;
 	esac
 }
 
 bootstrap() # no args
 {
-	msg "正在安装系统..."
-	createdir "$C_ROOT"
-	print_run debootstrap "$DB_SUITE" "$C_ROOT" "$DB_MIRROR" || \
-	  error "安装系统失败"
+	msg "$(M bootstrapping)"
+	createdir $C_ROOT
+	print_run debootstrap $DB_SUITE $C_ROOT $DB_MIRROR || \
+	  error "$(M bootstrap.failed)"
 }
 
 c_run() #(string[] cmdline)
 {
-	msg "[进入容器] 命令行：'$*'" GREEN
-	createdir "$C_ROOT/tmp" "$OPU_DIR" "$O_DATA"
-	LD_PRELOAD="" \
-	  proot -0 --link2symlink --kill-on-exit -r "$C_ROOT" -w "/root" \
-	  -b "/dev" -b "/proc" -b "/sys" -b "$C_ROOT/tmp:/dev/shm" \
-	  -b "$D_NULL:/proc/partitions" \
-	  -b "$DIR:$R_DIR" \
-	  -b "/apex" -b "/system" -b "/linkerconfig/ld.config.txt" \
-	  -b "$T_STOR" \
-	  -b "$T_ROOT/usr/tmp:/tmp" \
-	  -b "/sdcard" \
-	  -b "$OPU_DIR:$R_DIR/.openutau" \
-	  -b "$O_DATA:/root/.local/share/OpenUtau" \
-	  /bin/env -i TERM="$TERM" HOME="/root" RUNTIME_DIR="$R_DIR" \
-	  PATH="/usr/bin:/usr/sbin:$R_DIR/bin" LANG="zh_CN.UTF-8" \
-	  DISPLAY="$X_DISPLAY" GALLIUM_DRIVER=virpipe MESA_GL_VERSION_OVERRIDE=3.2 \
-	  PULSE_SERVER="tcp:127.0.0.1:4713" \
+	msg "$(M cont.run "$*")" GREEN
+	createdir $C_ROOT/tmp $OPU_DIR $HOME_DIR $O_DATA
+	LD_PRELOAD= \
+	  proot -0 --link2symlink --kill-on-exit -r $C_ROOT -w /root \
+	  -b /dev -b /proc -b /sys -b $C_ROOT/tmp:/dev/shm \
+	  -b $D_NULL:/proc/partitions \
+	  -b $DIR:$R_DIR \
+	  -b /apex -b /system -b /linkerconfig/ld.config.txt \
+	  -b $T_STOR \
+	  -b $T_ROOT/usr/tmp:/tmp \
+	  -b /sdcard \
+	  -b $OPU_DIR:$R_DIR/.openutau \
+	  -b $HOME_DIR:/root \
+	  -b $O_DATA:/root/.local/share/OpenUtau \
+	  /bin/env -i TERM=$TERM HOME=/root RUNTIME_DIR=$R_DIR \
+	  PATH=/usr/bin:/usr/sbin:$R_DIR/bin LANG=zh_CN.UTF-8 \
+	  DISPLAY=$X_DISPLAY GALLIUM_DRIVER=virpipe MESA_GL_VERSION_OVERRIDE=3.2 \
+	  PULSE_SERVER=tcp:127.0.0.1:4713 \
 	  DOTNET_GCHeapHardLimitPercent=50 \
-	  "$@"
+	  $@
 }
 
 host_svc() #(service)
 {
-	case "$1" in
-	x11) termux-x11 "$X_DISPLAY";;
+	case $1 in
+	x11) termux-x11 $X_DISPLAY;;
 	virgl) virgl_test_server_android;;
-	pulse)	DISPLAY="$X_DISPLAY" pulseaudio --exit-idle-time=-1 \
-		  -n -F "$RES/pulse-config.pa";;
+	pulse)	DISPLAY=$X_DISPLAY pulseaudio --exit-idle-time=-1 \
+		  -n -F $RES/pulse-config.pa;;
 	esac
 }
 
 ####### autoexec here
 
-## load lang
-
-. 
+## fixme
+load_lang "${LANG_OVERRIDE-zh_CN.UTF-8}"
 
 [ -w /sdcard ] && STOR_ACCESS=1 || STOR_ACCESS=0
-[ $STOR_ACCESS = 1 ] || msg "[警告] 存储权限未授予，可能影响使用" YELLOW
+[ $STOR_ACCESS = 1 ] || msg "$(M stor.denied)" YELLOW
--- t.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-echo "$0"
-. ./test.sh
-
